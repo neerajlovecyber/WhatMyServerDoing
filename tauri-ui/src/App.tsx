94644b0f8b5d2cb6aa7b1d453f9ea1a1677da610
Neeraj singh <nirmalapanghal43@gmail.com>
2024-05-28 00:45:38 +0530

    template

---
+++ tauri-ui/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import { invoke } from "@tauri-apps/api/tauri"
+
+import { Menu } from "@/components/menu"
+
+import { TailwindIndicator } from "./components/tailwind-indicator"
+import { ThemeProvider } from "./components/theme-provider"
+import DashboardPage from "./dashboard/page"
+import { cn } from "./lib/utils"
+
+function App() {
+ 
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="h-screen overflow-clip">
+        <Menu />
+        <div
+          className={cn(
+            "h-screen overflow-auto border-t bg-background pb-8",
+            // "scrollbar-none"
+            "scrollbar scrollbar-track-transparent scrollbar-thumb-accent scrollbar-thumb-rounded-md"
+          )}
+        >
+          <DashboardPage />
+        </div>
+      </div>
+      <TailwindIndicator />
+    </ThemeProvider>
+  )
+}
+
+export default App
