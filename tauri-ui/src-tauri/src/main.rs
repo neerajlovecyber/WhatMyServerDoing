b13cd74d701bfdd40104fee022bea86860ad77bd
Neeraj singh <nirmalapanghal43@gmail.com>
2024-06-09 15:51:09 +0530

    sda

--- tauri-ui/src-tauri/src/main.rs
+++ tauri-ui/src-tauri/src/main.rs
@@ -9,14 +9,17 @@
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
-
+use tauri::{ SystemTray, SystemTrayMenu};
 fn main() {
+    let tray_menu = SystemTrayMenu::new(); // insert the menu items here
+    let system_tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .plugin(tauri_plugin_app::init())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_window::init())
+        .system_tray(system_tray)
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
